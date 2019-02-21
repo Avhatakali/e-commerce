171d33d77d100cf6693258233532d6ac0ae686f3
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-02-21 11:00:08 +0200

    added validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :name, presence: true,
+    length: {minimum: 4 }
+
+  validates :price, :quantity, presence: true,
+    length: {minimum: 2 }
 end
