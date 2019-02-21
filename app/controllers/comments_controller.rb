36666cf71c47e766d89560400ca180782e96bc7b
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-02-21 10:14:48 +0200

    indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,45 +1,45 @@
 class CommentsController < ApplicationController
 
-    def new
-      @user = current_user
-      @product = Product.find(params[:product_id])
-      @comment = @product.comments.new
+  def new
+    @user = current_user
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @product = @comment.product
+  end
+
+  def create
+    @user = current_user
+    @comment = @user.comments.create(comment_params)
+
+    if @comment.save
+      redirect_to products_path(@product)
+    else
+      render 'new'
     end
+  end
 
-    def edit
-      @comment = Comment.find(params[:id])
-      @product = @comment.product
+  def update
+    @comment = Comment.find(params[:id])
+    @product = @comment.product
+
+    if @comment.update(comment_params)
+      redirect_to products_path(@product)
+    else
+      render 'edit'
     end
+  end
 
-    def create
-      @user = current_user
-      @comment = @user.comments.create(comment_params)
+  def destroy
+    @comment = Comment.find(params[:id])
+    @product = @comment.product
 
-      if @comment.save
-        redirect_to products_path(@product)
-      else
-        render 'new'
-      end
-    end
-
-    def update
-      @comment = Comment.find(params[:id])
-      @product = @comment.product
-
-      if @comment.update(comment_params)
-        redirect_to products_path(@product)
-      else
-        render 'edit'
-      end
-    end
-
-    def destroy
-      @comment = Comment.find(params[:id])
-      @product = @comment.product
-
-      @comment.destroy
-      redirect_to product_path(@product)
-    end
+    @comment.destroy
+    redirect_to product_path(@product)
+  end
 
   private
     def comment_params
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,49 +1,49 @@
 class ProductsController < ApplicationController
 
-    def index
-      @products = Product.all
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @user = current_user
+    @product = @user.products.create(product_params)
+
+    if @product.save
+      redirect_to products_path(@product)
+    else
+      render 'new'
     end
+  end
 
-    def show
-      @product = Product.find(params[:id])
-    end
+  def update
+    @product = Product.find(params[:id])
 
-    def new
-      @user = current_user
-      @product = Product.new
-    end
-
-    def edit
-      @product = Product.find(params[:id])
-    end
-
-    def create
-      @user = current_user
-      @product = @user.products.create(product_params)
-
-      if @product.save
-        redirect_to products_path(@product)
-      else
-        render 'new'
-      end
-    end
-
-    def update
-      @product = Product.find(params[:id])
-
-      if @product.update(product_params)
-        redirect_to products_path
-      else
-        render 'edit'
-      end
-    end
-
-    def destroy
-      @product = Product.find(params[:id])
-      @product.destroy
-
+    if @product.update(product_params)
       redirect_to products_path
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
 
   private
     def product_params
