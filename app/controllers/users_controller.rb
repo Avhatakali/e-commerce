8e46ce59f211cadecee0fd5b3a162fd7c5e8467a
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-03-05 16:24:55 +0200

    user show page done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    @track_transactions = TrackTransaction.where(buyer: current_user)
+    @user_login = current_user
+    @track1_transactions = TrackTransaction.where(buyer: current_user)
+    @track_transactions = TrackTransaction.where(seller: current_user)
   end
 end
